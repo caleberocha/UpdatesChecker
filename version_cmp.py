aa1646dafa72c4ae2703e3b1517220a0d8ad7f8e
Calebe Rocha <angelo.calebe@acad.pucrs.br>
2019-10-23 14:35:07 -0300

    Comparador de versões: Ajustes menores

--- version_cmp.py
+++ version_cmp.py
@@ -4,29 +4,20 @@ class Version:
     def __init__(self, version, delimiter="."):
         self.version = version
         self.delimiter = delimiter
-        # self.version_int = self._convert(self.version)
-        # print(self.version_int)
-
-    def _convert(self, version):
-        len_version_part = 3
-        v = version.split(".")
-        for p in v[1:]:
-            if len(p) > len_version_part:
-                len_version_part = len(p)
-        
-        divisor = 10**int(len_version_part) #((len_version_part//3+1)*3)
-
-        version_int = float(v[0])
-        for i, p in enumerate(v[1:]):
-            version_int += int(p) / ((i+1) * divisor)
-        
-        return version_int
 
     def __eq__(self, other):
         return self.version == other.version
 
     def __ne__(self, other):
         return self.version != other.version
+
+    def __lt__(self, other):
+        for i in zip_longest(self.version.split(self.delimiter), other.version.split(self.delimiter), fillvalue=-2147483648):
+            if int(i[0]) < int(i[1]):
+                return True
+            elif int(i[0]) > int(i[1]):
+                return False
+        return False
 
     def __le__(self, other):
         for i in zip_longest(self.version.split(self.delimiter), other.version.split(self.delimiter), fillvalue=-2147483648):
@@ -36,6 +27,14 @@ class Version:
                 return False
         return True
 
+    def __gt__(self, other):
+        for i in zip_longest(self.version.split(self.delimiter), other.version.split(self.delimiter), fillvalue=-2147483648):
+            if int(i[0]) > int(i[1]):
+                return True
+            elif int(i[0]) < int(i[1]):
+                return False
+        return False    
+
     def __ge__(self, other):
         for i in zip_longest(self.version.split(self.delimiter), other.version.split(self.delimiter), fillvalue=-2147483648):
             if int(i[0]) > int(i[1]):
@@ -43,22 +42,6 @@ class Version:
             elif int(i[0]) < int(i[1]):
                 return False
         return True
-
-    def __lt__(self, other):
-        for i in zip_longest(self.version.split(self.delimiter), other.version.split(self.delimiter), fillvalue=-2147483648):
-            if int(i[0]) > int(i[1]):
-                return False
-            elif int(i[0]) < int(i[1]):
-                return True
-        return False
-
-    def __gt__(self, other):
-        for i in zip_longest(self.version.split(self.delimiter), other.version.split(self.delimiter), fillvalue=-2147483648):
-            if int(i[0]) < int(i[1]):
-                return False
-            elif int(i[0]) > int(i[1]):
-                return True
-        return False
 
 
 if __name__ == "__main__":
